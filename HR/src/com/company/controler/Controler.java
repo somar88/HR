df62746de6e36952a9f2e4345a92eca516a8b33b
somar <somar@Extreme-PC>
2017-02-11 18:07:48 +0100

    1

--- HR/src/com/company/controler/Controler.java
+++ HR/src/com/company/controler/Controler.java
@@ -5,7 +5,6 @@ public class Controler {
 	public static void main(String[] args) {
 		System.out.println("testing GITHUB");
 		System.out.println("bitch plese!");
-		
 		System.out.println("I love pico!");
 		
 	}
