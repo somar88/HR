dc19611e6de79e60129080422f61c1cededb42fd
somar <somar@Extreme-PC>
2017-02-12 16:42:21 +0100

    person - more variables

--- HR/src/com/company/controler/Controler.java
+++ HR/src/com/company/controler/Controler.java
@@ -3,9 +3,6 @@ package com.company.controler;
 public class Controler {
 
 	public static void main(String[] args) {
-		System.out.println("testing GITHUB");
-		System.out.println("bitch plese!");
-		System.out.println("I love pico!");
 		
 	}
 
--- HR/src/com/company/model/Person.java
+++ HR/src/com/company/model/Person.java
@@ -1,5 +1,7 @@
 package com.company.model;
 
+import java.util.Date;
+
 public class Person {
 
 	private String first_name;
@@ -7,17 +9,25 @@ public class Person {
 	private String middle_name;
 	private Address addresses;
 	private Phone phone_numbers;
+	private Gender gender;
+	private String social_security_num;
+	private Date birth_day;
+	private String birth_place;
 
 	public Person() {
 		this.first_name = "First";
 		this.last_name = "Last";
 		this.middle_name = "Middle";
+		this.gender = Gender.NATURAL;
+		this.social_security_num = "";
+		this.birth_day = new Date();
+		this.birth_place = "N/A";
 		this.addresses = new Address();
 		this.phone_numbers = new Phone();
 	}
 
 	public String toString() {
-		return "First" + this.first_name + "\n" + 
+		return  "First" + this.first_name + "\n" + 
 				"Last" + this.last_name + "\n" + 
 				"Middle" + this.middle_name + "\n"+ 
 				"Phone: \n" + this.phone_numbers.toString() + "\n" + 
@@ -47,5 +57,43 @@ public class Person {
 	public void setMiddle_name(String middle_name) {
 		this.middle_name = middle_name;
 	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getSocial_security_num() {
+		return social_security_num;
+	}
+
+	public void setSocial_security_num(String social_security_num) {
+		this.social_security_num = social_security_num;
+	}
+
+	public Date getBirth_day() {
+		return birth_day;
+	}
+
+	public void setBirth_day(Date birth_day) {
+		this.birth_day = birth_day;
+	}
+
+	public String getBirth_place() {
+		return birth_place;
+	}
+
+	public void setBirth_place(String birth_place) {
+		this.birth_place = birth_place;
+	}
+
+}
+
+enum Gender {
+
+	FEMAL, MALE, NATURAL
 
 }
